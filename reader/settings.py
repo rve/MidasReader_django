37490cd8b8ac7e4dc8345da5b8839075baab2915
rve <jiongrr@gmail.com>
2012-05-31 17:59:56 +0800

    Update reader/settings.py

--- reader/settings.py
+++ reader/settings.py
@@ -136,7 +136,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'read',
     'dajaxice',
-	'dajax',
+    'dajax',
     #'bookreader',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
