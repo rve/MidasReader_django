15f990fb136d8d94b7666602ee709152e2bcc1be
Mo Yijie <nemomojie@iFedora.(none)>
2012-06-07 22:29:28 +0800

    delete PC gesture and beautitify personal.html

--- static/js/gesture.js
+++ static/js/gesture.js
@@ -3,53 +3,17 @@ var SupportsTouches;
 function onLoadEvent()
 {
   _points = new Array();
-
   SupportsTouches=("createTouch" in document);
-  var canvas = document.getElementById('contentId');
-  _rc = getCanvasRect(canvas); 
   _isDown = false;
-  var      StartEvent = SupportsTouches ? "touchstart" : "mousedown",
-  MoveEvent = SupportsTouches ? "touchmove" : "mousemove",
-  EndEvent = SupportsTouches ? "touchend" : "mouseup";
-  $("#container").bind(StartEvent,function(event){
-    // alert(StartEvent);
-    mousePos=getMousePoint(event);
-    mouseDownEvent(event.clientX,event.clientY);
-  });
-  $("#container").bind(MoveEvent,function(event){
-    // alert(MoveEvent);
-    mousePos=getMousePoint(event);
-    mouseMoveEvent(event.clientX,event.clientY);
-  });
-  $("#container").bind(EndEvent,function(){
-    // alert(EndEvent);
-    mousePos=getMousePoint(event);
-    mouseUpEvent(event.clientX,event.clientY);
-  })
-
-  if (!SupportsTouches)
-    {
-      $("#container").bind(StartEvent,function(event){
-        mousePos=getMousePoint(event);
-        mouseDownEvent(event.clientX,event.clientY);
-      });
-      $("#container").bind(MoveEvent,function(event){
-        mousePos=getMousePoint(event);
-        mouseMoveEvent(event.clientX,event.clientY);
-      });
-      $("#container").bind(EndEvent,function(){
-        mousePos=getMousePoint(event);
-        mouseUpEvent(event.clientX,event.clientY);
-      })
-    }
-    else
-      {
-        var goal=document.getElementById("container");
-        goal.addEventListener("touchstart",touchStart,false);
-        goal.addEventListener("touchmove",touchMove,false);
-        goal.addEventListener("touchend",touchEnd,false);
-        goal.addEventListener("gestureend",gestureEnd,false);
-      }
+  var goal=document.getElementById("container");
+  goal.addEventListener("touchstart",touchStart,false);
+  goal.addEventListener("touchmove",touchMove,false);
+  goal.addEventListener("touchend",touchEnd,false);
+  if (!SupportsTouches) $("#container").bind("click",function(event)
+                                             {
+                                               mousePos={x:event.clientX,y:event.clientY};
+                                               comment_show();
+                                             })
 }
 
 var startX,startY;
@@ -94,9 +58,10 @@ function touchEnd(ev)
               }
         }
     }
+    else comment_show();
 }
 
-function gestureEnd()
+function comment_show()
 {
   LastMousePos=mousePos;
   $(".C_and_N").slideToggle("slow");
@@ -137,102 +102,12 @@ function getMousePoint(ev)
         return {'x' : x, 'y' : y};
 };
 
-function getCanvasRect(canvas)
-{
-  var w = canvas.width;
-  var h = canvas.height;
-
-  var cx = canvas.offsetLeft;
-  var cy = canvas.offsetTop;
-  while (canvas.offsetParent != null)
-    {
-      canvas = canvas.offsetParent;
-      cx += canvas.offsetLeft;
-      cy += canvas.offsetTop;
-    }
-    return {x: cx, y: cy, width: w, height: h};
-}
-
-function getScrollY()
-{
-  var scrollY = 0;
-  if (typeof(document.body.parentElement) != 'undefined')
-    {
-      scrollY = document.body.parentElement.scrollTop; // IE
-    }
-    else if (typeof(window.pageYOffset) != 'undefined')
-      {
-        scrollY = window.pageYOffset; // FF
-      }
-      return scrollY;
-}
-//
-// Mouse Events
-//
-var mousePos;
-
-function mouseDownEvent(x, y)
-{
-  x -= _rc.x;
-  y -= _rc.y - getScrollY();
-  _points.length = 1;
-  _points[0] = new Point(x, y);
-  _isDown = true;
-}
-
 function distance(a,b)
 {
   return Math.sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
 }
-function mouseMoveEvent(x,y)
-{
-  if (distance(mousePos,LastMousePos)>50 && (!$(".C_and_N").is("hidden")))
-    {
-      $(".C_and_N").hide();
-    }
-    if (_isDown)
-      {
-        x -= _rc.x;
-        y -= _rc.y - getScrollY();
-        _points[_points.length] = new Point(x, y); // append
-      }
-}
 
 var LastMousePos={x:0,y:0};
-
-function mouseUpEvent(x, y)
-{
-  if (_isDown)
-    {
-      _isDown = false;
-      if (_points.length >= 10)
-        {
-          var result=judge_gesture();
-          if (result=="Line" && 
-              distance(_points[0],_points[_points.length-1])>100)
-            {
-              if (_points[0].x<_points[_points.length-1].x)
-                {
-                  document.getElementById("next_page").click();
-                }
-                else document.getElementById("prev_page").click();
-            }
-        }
-        else // fewer than 10 points were inputted
-          {
-            LastMousePos=mousePos;
-            $(".C_and_N").slideToggle("slow");
-            console.log(mousePos.x);
-            document.getElementById("CNID").style.left=mousePos.x.toString(10)+"px";
-            document.getElementById("CNID").style.top=mousePos.y.toString(10)+"px";
-          }
-    }
-}
-function round(n, d) // round 'n' to 'd' decimals
-{
-  d = Math.pow(10, d);
-  return Math.round(n * d) / d
-}
 
 function judge_gesture()
 {
@@ -243,15 +118,5 @@ function judge_gesture()
     if (Math.abs(_points[i].y-_points[i-1].y)>maxDy) maxDy=Math.abs(_points[i].y-_points[i-1].y);
   }
   if (maxDy<10) return "Line";
-  if (maxDy<10) return "Col";
-  if (_points[n-1].x>_points[0].x)
-    {
-      if (_points[n-1].y>_points[0].y) return "LU_RD";
-      else return "LD_RU";
-    }
-    else
-      {
-        if (_points[n-1].y>_points[0].y) return "RU_LD";
-        else return "RD_LU";
-      }
+  return "Others"
 }
